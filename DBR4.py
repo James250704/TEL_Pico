c7e14489188c137d957d56f4f18c28fba5d6d98c
James250704 <James20050704@gmail.com>
2025-09-26 22:36:17 +0800

    更改腳位設定

--- DBR4.py
+++ DBR4.py
@@ -2,7 +2,7 @@ from machine import UART
 import time
 
 # ===== UART 初始化 =====
-uart = UART(0, baudrate=420000, tx=12, rx=13, bits=8, parity=None, stop=1)
+uart = UART(1, baudrate=420000, tx=8, rx=9, bits=8, parity=None, stop=1)
 
 # ===== CRSF 常數 =====
 CRSF_SYNC = 0xC8
